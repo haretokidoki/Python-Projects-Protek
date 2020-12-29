3a87b2547d442d96148a6539363903d674364bd8
Natanael Junior Setiawan <natanaeljunior582@gmail.com>
2020-12-29 06:51:43 +0000

    Chapter 10 29/12/2020

--- chapter_10/project/project05.py
+++ chapter_10/project/project05.py
@@ -1,22 +1,21 @@
 # cek apakah file ada
+path = 'txt_file/angkagaris.txt'
+
 try:
-  filein = open('txt_file/angkagaris.txt','r')
+  filein = open(path,'r')
 except FileNotFoundError:
   print("File tidak ditemukan!")
 
-#input file tujuan
-filex = input("Masukkan nama file tujuan : ")
-
-# buka file tujuan
-fileout = open('txt_file/'+filex+'.txt','a')
+# buat file tujuan
+fileout = open(path[:len(path)-4]+"_hasil.txt",'w+')
 
 # menambahkan angka dari variabel a+b
 def numberCounter(filein, fileout):
   for x in filein:
     number = x.split('|')
-    a = number[0]
-    b = number[1]
-    fileout.write(a+b)
+    a = int(number[0])
+    b = int(number[1])
+    fileout.write(str(a+b)+"\n")
   print("Output sudah disimpan di", fileout.name)
 
 numberCounter(filein, fileout)
